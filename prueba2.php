0dab4e07127c3147d2e8dbabd01bf69568fd6534
roberto <robertoe.alcantara@gmail.com>
2015-12-08 18:14:18 -0600

    prueba2.phpcambio

---
+++ prueba2.php
@@ -0,0 +1,22 @@
+<?php
+echo "hola";
+
+/**
+ * <<<HTML
+ content here
+ HTML;
+ 
+ */
+class ClassName extends AnotherClass {
+	git $arrayName = array('' => , );
+	function __construct($argument) {
+		
+		
+	//pero le ando cambiando
+		
+	}
+}
+
+
+
+ ?>
